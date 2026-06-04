eeb71bdb673c7925ba3c0b35a959185a5973028d
Anshika <anshika25aneja09@gmail.com>
2026-06-04 11:05:12 +0530

    Initiated MCP Server

---
+++ weather-mcp-server/src/main/java/com/anshika/weathermcp/WeatherMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.anshika.weathermcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherMcpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherMcpServerApplication.class, args);
+	}
+
+}
---
+++ weather-mcp-server/src/test/java/com/anshika/weathermcp/WeatherMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anshika.weathermcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherMcpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
