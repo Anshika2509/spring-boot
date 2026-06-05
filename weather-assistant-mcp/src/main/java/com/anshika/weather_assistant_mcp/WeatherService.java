2a9f9e6102735c743163f6895c29680851145fc1
Anshika <anshika25aneja09@gmail.com>
2026-06-05 16:39:13 +0530

    Forecast request

--- weather-assistant-mcp/src/main/java/com/anshika/weather_assistant_mcp/WeatherService.java
+++ weather-assistant-mcp/src/main/java/com/anshika/weather_assistant_mcp/WeatherService.java
@@ -3,6 +3,7 @@ package com.anshika.weather_assistant_mcp;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -24,6 +25,40 @@ public class WeatherService {
             .body(Map.class);
         Map properties = (Map) response.get("properties");
         String forecastUrl = properties.get("forecast").toString();
-        return forecastUrl;
+
+        Map forecastResponse = restClient.get()
+                .uri(forecastUrl)
+                .retrieve()
+                .body(Map.class);
+
+        Map forecastProperties = (Map) forecastResponse.get("properties");
+        List periods = (List) forecastProperties.get("periods");
+        Map firstPeriod = (Map) periods.get(0);
+
+        String name = firstPeriod.get("name").toString();
+        String temperature = (firstPeriod.get("temperature").toString()) + "°" + firstPeriod.get("temperatureUnit").toString();
+        String forecast = firstPeriod.get("detailedForecast").toString();
+
+        System.out.println("""
+        %s
+        Temperature: %s
+
+        %s
+        """.formatted(
+                name,
+                temperature,
+                forecast
+        ));
+
+        return """
+        <h2>%s</h2>
+        <p>Temperature: %s</p>
+        <p>%s</p>
+        """.formatted(
+                name,
+                temperature,
+                forecast
+        );
+
     }
 }
